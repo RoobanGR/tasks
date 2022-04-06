44d5df77c582cd902f2317f1b774211e31b7cd7d
tringapps-roobanGR <96728739+tringapps-roobanGR@users.noreply.github.com>
2022-04-06 17:05:36 +0530

    Update script.js

--- task8/script.js
+++ task8/script.js
@@ -1,23 +1,19 @@
 
 var myNodelist = document.getElementsByTagName("LI");
 var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
+var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
-  span.className = "close";
+for (i = 0; i < myNodelist.length; i++) {
+   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
 var close = document.getElementsByClassName("close");
-var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
     storage.removeItem(div);
-
-
-   
   }
 }
 
@@ -45,9 +41,6 @@ function newElement() {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
